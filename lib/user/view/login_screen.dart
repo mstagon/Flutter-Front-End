703171e3fc314089725b4ec451b3f8682916bd69
mstagon <devcms21@gmail.com>
2024-11-21 20:30:10 +0900

    initial commit

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dimple/common/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -5,8 +6,33 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('로그인 스크린'),
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 177,),
+            Container(
+              child: Column(
+                children: [
+                  Image.asset('assets/img/banreou.png', width: 277, height: 277,),
+                ],
+              ),
+            ),
+            SizedBox(height: 181,),
+            GestureDetector(child: Image.asset('assets/img/login/kakao.png',  width: 336, height: 56,),
+              onTap: () {
+              //Navigator.push(context, MaterialPageRoute(builder: (context) => KakaoLoginScreen()));
+              },),
+            SizedBox(height: 18,),
+            GestureDetector(child: Image.asset('assets/img/login/google.png',  width: 336, height: 56,),
+              onTap: () {
+              //Navigator.push(context, MaterialPageRoute(builder: (context) => GoogleLoginScreen()));
+              },),
+          ],
+
+        ),
+      ),
     );
   }
 }
