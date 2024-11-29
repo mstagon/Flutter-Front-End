85541f0375420a54c3dca5f0e10f9ed88221525b
mstagon <devcms21@gmail.com>
2024-11-29 17:06:34 +0900

    Feat: "산책 페이지" 상태관리 및 디자인 완성했습니다.
    TODO : 백엔드 엔드포인트 입력만 남음

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("apikey")
+    GMSServices.provideAPIKey("AIzaSyBTixqzbJnTbgcvZt43aRZgnmnMOre5gi0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
